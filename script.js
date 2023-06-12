fd5bfded6215e4ce4e6586cd9d0eae2738185a4c
mateusscarin <mateus.scarin@gmail.com>
2023-06-12 13:49:24 -0300

    feat: funções de cálculo de tamanho de amostra pela média e por proporção

--- script.js
+++ script.js
@@ -63,34 +63,50 @@ const montaParametros = () => {
             break;
         case 2:
             parametros.innerHTML = `
-            <div><label>Média:</label>
-            <input type="text"> </div>
-            <div><label>Desvio padrão:</label>
-            <input type="text"></div>
             <div>
-            <label>Grau de confiança:</label>
-            <div><input type="radio" name="grauConfianca" value="1.645"><span>90%</span></div>
-            <div><input type="radio" name="grauConfianca" value="1.96"><span>95%</span></div>
-            <div><input type="radio" name="grauConfianca" value="2.575"><span>99%</span></div>
+                <label>Desvio padrão:</label>
+                <input type="text" id="desvioPadrao">
             </div>
-            <div><label>Tamanho da amostra:</label>
-            <input type="text"> </div>
+            <div>
+                <label>Erro médio:</label>
+                <input type="text" id="erroMedio">
+            </div>
+            <div>
+                <label>Grau de confiança:</label>
+                <div>
+                    <input type="radio" name="grauConfianca" value="1.645"><span>90%</span>
+                </div>
+                <div>
+                    <input type="radio" name="grauConfianca" value="1.96"><span>95%</span>
+                </div>
+                <div>
+                    <input type="radio" name="grauConfianca" value="2.575"><span>99%</span>
+                </div>
+            </div>
             <input type="submit" onclick="mostraResultado()">`;
             break;
         case 3:
             parametros.innerHTML = `
-            <div><label>Média:</label>
-            <input type="number"> </div>
-            <div><label>Desvio padrão:</label>
-            <input type="number" ></div>
             <div>
-            <label>Grau de confiança:</label>
-            <div><input type="radio" name="grauConfianca" value="1.645"><span>90%</span></div>
-            <div><input type="radio" name="grauConfianca" value="1.96"><span>95%</span></div>
-            <div><input type="radio" name="grauConfianca" value="2.575"><span>99%</span></div>
+                <label>Número de sucesso:</label>
+                <input type="text" id="numSucesso">
             </div>
-            <div><label>Tamanho da amostra:</label>
-            <input type="number" id="tamanhoAmostra"> </div>
+            <div>
+                <label>Erro proporcional:</label>
+                <input type="text" id="erroProp">
+            </div>
+            <div>
+                <label>Grau de confiança:</label>
+                <div>
+                    <input type="radio" name="grauConfianca" value="1.645"><span>90%</span>
+                </div>
+                <div>
+                    <input type="radio" name="grauConfianca" value="1.96"><span>95%</span>
+                </div>
+                <div>
+                    <input type="radio" name="grauConfianca" value="2.575"><span>99%</span>
+                </div>
+            </div>
             <input type="submit" onclick="mostraResultado()">`;
             break;
         default:
@@ -108,24 +124,30 @@ const mostraResultado = () => {
         case 0:
             const desvioPadrao = Number(document.getElementById('desvioPadrao').value.replace(',', '.'));
             const media = Number(document.getElementById('media').value.replace(',', '.'));
-            const tamanhoAmostra = Number(document.getElementById('tamanhoAmostra').value.replace('.', ','));
+            const tamanhoAmostra = Number(document.getElementById('erroMedio').value.replace('.', ','));
             let erroMed = calcularErroMedia(grauConfianca, desvioPadrao, tamanhoAmostra);
             resultado.innerHTML = `O erro médio é: ${(erroMed)}</br>
                                 Intervalo de confiança: ${(media - erroMed)} < μ < ${(media + erroMed)}`;
             break;
         case 1:
-            const p = Number(document.getElementById('numSucesso').value.replace(',', '.') 
-            / document.getElementById('tamanhoAmostra').value.replace(',', '.'));
+            const p = Number(document.getElementById('numSucesso').value.replace(',', '.')
+                / document.getElementById('tamanhoAmostra').value.replace(',', '.'));
             const tamanhoAmostra2 = Number(document.getElementById('tamanhoAmostra').value.replace('.', ','));
             let erroProp = calcularErroProporcao(grauConfianca, p, tamanhoAmostra2);
-            resultado.innerHTML = `O erro da proporção é: ${(erroProp)}</br>
+            resultado.innerHTML = `O erro proporçional é: ${(erroProp)}</br>
                                 Intervalo de confiança: ${(p - erroProp)} < μ < ${(p + erroProp)}`;
             break;
         case 2:
-            resultado.innerHTML = `O resultado é: 2,4`;
+            const desviPadrao = Number(document.getElementById('desvioPadrao').value.replace(',', '.'));
+            const erroMedio = Number(document.getElementById('erroMedio').value.replace(',', '.'));
+            console.log(Number(document.getElementById('erroMedio').value.replace(',', '.')))
+            resultado.innerHTML = `Tamanho da amostra: ${calcularTamanhoAmostraPelaMed(grauConfianca, desviPadrao, erroMedio)}`;
+            console.log(calcularTamanhoAmostraPelaMed(grauConfianca, desviPadrao, erroMedio));
             break;
         case 3:
-            resultado.innerHTML = `O resultado é: 2,4`;
+            const p2 = Number(document.getElementById('numSucesso').value.replace(',', '.'));
+            const erroProporc = Number(document.getElementById('erroProp').value.replace(',', '.'));
+            resultado.innerHTML = `Tamanho da amostra: ${calcularTamanhoAmostraPelaProp(grauConfianca, p2, erroProporc)}`;
             break;
         default:
             console.log(formulaSelecionada);
@@ -139,4 +161,12 @@ const calcularErroMedia = (grauConf, desvPad, tamAmostra) => {
 
 const calcularErroProporcao = (grauConf, p, tamAmostra) => {
     return grauConf * Math.sqrt((p * (1 - p)) / tamAmostra);
+}
+
+const calcularTamanhoAmostraPelaMed = (grauConf, desvPad, erroMed) => {
+    return Math.round(Math.pow((grauConf * desvPad) / erroMed, 2) + 0.6);
+}
+
+const calcularTamanhoAmostraPelaProp = (grauConf, p, erroProp) => {
+    return Math.round((Math.pow(grauConf, 2) * p * (1 - p)) / Math.pow(erroProp, 2) + 0.6);
 }
